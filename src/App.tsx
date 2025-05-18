fd44a51c6054948791cdae161f3a8ddd9f233dcd
atishay46 <mysto.x09@gmail.com>
2025-05-19 00:14:55 +0530

    Fix: type errors for Spline component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import { useCallback } from 'react';
 import Spline from '@splinetool/react-spline';
-import type { SplineEvent } from '@splinetool/react-spline';
 import './App.css';
 
 function App() {
-  const onLoad = useCallback((splineApp: any) => {
+  const onLoad = useCallback(() => {
     console.log('Spline scene loaded');
   }, []);
 
   const onMouseDown = useCallback((e: any) => {
     if (e.target?.name) {
       console.log('Clicked on object:', e.target.name);
+      // Handle object click here (e.g., open modal, show project preview, etc.)
+    }
+  }, []);
+
+  const onMouseOver = useCallback((e: any) => {
+    if (e.target?.name) {
+      console.log('Hovering over object:', e.target.name);
     }
   }, []);
 
@@ -20,6 +26,7 @@ function App() {
         scene="https://prod.spline.design/aQiimP0gzGY3cfho/scene.splinecode"
         onLoad={onLoad}
         onMouseDown={onMouseDown}
+        onMouseOver={onMouseOver}
       />
     </div>
   );
